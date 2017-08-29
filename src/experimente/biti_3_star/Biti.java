aa05cab5b8650722cd17e31445c7e9b1cc4a179f
Silviu <xsylver@yahoo.com>
2017-08-30 00:12:44 +0300

    updated recursive backtracking code

--- src/experimente/biti_3_star/Biti.java
+++ src/experimente/biti_3_star/Biti.java
@@ -1,57 +1,48 @@
 package experimente.biti_3_star;
 
-import java.util.Arrays;
-
 import static java.lang.System.out;
 
 
 public class Biti {
 
-    private static int n = 3;
+    private static int n = 4;
     private static int min_len = (int) Math.pow(2, n) + n - 1;
-    private static Integer[] sol = new Integer[100];
-    private static boolean[] visited = new boolean[100];
+//    private static Integer[] sol = new Integer[100];
+    private static String sol = "";
 
     private static void print() {
-        Arrays.stream(sol).filter(e -> e != null).forEach(out::print);
+        System.out.print(sol);
         out.println();
     }
 
-    private static boolean isOptimal(int step) {
-        if (step < n) return true;
-        int lastNumber = getNumberFromIndex(step);
-        if (visited[lastNumber]) {
-//            System.out.print("Found a bad solution: ");
-//            print();
-            return false;
-        }
-        visited[lastNumber] = true;
-        return true;
+    private static boolean isValid(int i, int step) {
+        //check for a valid step
+        if (step <= n - 1) return true;
+        //get the last number
+        String lastNumber = sol.substring(step - n + 1, step) + i;
+        //sol is valid if it does not contain last number
+        return !sol.contains(lastNumber);
     }
 
-    private static int getNumberFromIndex(int index) {
-        int lastNumber = 0;
-        for (int j = index; j >= index - n + 1; --j) {
-            lastNumber += sol[j] * (int) Math.pow(2, index - j);
+    private static boolean back(int step) {
+
+        if (step == min_len - 1) {
+            print();
+            return true;
         }
-//        System.out.println(lastNumber);
-        return lastNumber;
-    }
-
-    private static void back(int step) {
-
-            for (int i = 0; i < 2; ++i) {
-                sol[step] = i;
-                if (step <= min_len && isOptimal(step)) {
-                    print();
+        for (int i = 0; i < 2; ++i) {
+            if (isValid(i, step)) {
+                sol = sol + i;
+                if (back(step + 1)) {
+                    return true;
                 }
-                back(step + 1);
+                sol = sol.substring(0, sol.length() - 1);
             }
-
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        visited[0] = true;
         back(0);
     }
 
