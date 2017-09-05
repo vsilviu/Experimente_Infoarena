b41edd2d5785b00bae1708581083a8481dac4303
Silviu <xsylver@yahoo.com>
2017-09-06 00:52:28 +0300

    Removed redundant code Used eulerian cycle idea and optimized algorithm

--- src/experimente/biti_2_star/Main.java
+++ src/experimente/biti_2_star/Main.java
@@ -5,48 +5,65 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Stack;
 
 
 public class Main {
 
-    private static int n;
-    private static int min_len;
-    private static String sol = "";
     private static Scanner reader;
     private static BufferedWriter writer;
 
-    private static boolean isValid(int i, int step, final String sol) {
-        String lastNumber = sol.substring(step - n + 1, step) + i;
-        return !sol.contains(lastNumber);
-    }
 
-    private static boolean back(int step, String sol) throws IOException {
-        if (step == min_len) {
-            writer.write(sol);
-            return true;
+    private static String deBrujinSequenceGenerator(int n) {
+        if (n == 0) {
+            return "01";
         }
-        for (int i = 0; i < 2; ++i) {
-            if (isValid(i, step, sol)) {
-                sol += i;
-                if (back(step + 1, sol)) return true;
-                sol = sol.substring(0, sol.length() - 1);
+
+        int VERTEX_COUNT = 2 << (n - 1);
+        StringBuilder solution = new StringBuilder();
+        boolean[] visited = new boolean[VERTEX_COUNT];
+        Stack<Integer> currPath = new Stack<>();
+        currPath.push(0);
+
+        int n0, n1;
+        int crtVertex = 0;
+
+        while (!currPath.isEmpty()) {
+            n0 = 2 * (crtVertex % (VERTEX_COUNT >> 1));
+            n1 = n0 + 1;
+            if (!visited[n0] || !visited[n1]) {
+                currPath.push(crtVertex);
+                if (!visited[n0]) {
+                    crtVertex = n0;
+                    visited[n0] = true;
+                } else {
+                    crtVertex = n1;
+                    visited[n1] = true;
+                }
+            } else {
+                solution.append(crtVertex & 1);
+                crtVertex = currPath.pop();
             }
         }
-        return false;
+
+        StringBuilder zero = new StringBuilder();
+
+        for(int i = 0; i < n - 1; ++i) {
+            zero.append(0);
+        }
+
+        return zero.append(solution.reverse()).toString();
     }
+
 
     public static void main(String[] args) throws IOException {
         reader = new Scanner(new FileInputStream("biti.in"));
         writer = new BufferedWriter(new FileWriter("biti.out"));
 
-        n = reader.nextInt();
-        min_len = (int) Math.pow(2, n) + n - 1;
-        writer.write(String.valueOf(min_len) + "\n");
-
-        for (int i = 0; i < n; ++i) {
-            sol += "0";
-        }
-        back(n, sol);
+        int n = reader.nextInt();
+        int minLen = (2 << (n - 1)) + n - 1;
+        writer.write(String.valueOf(minLen) + "\n");
+        writer.write(deBrujinSequenceGenerator(n));
 
         reader.close();
         writer.close();
--- src/experimente/biti_2_star/MainOptimizedV2.java
+++ src/experimente/biti_2_star/MainOptimizedV2.java
@@ -1,7 +1,6 @@
 package experimente.biti_2_star;
 
 import java.util.*;
-import java.util.stream.IntStream;
 
 /**
  * Created by Silviu on 9/3/17.
@@ -10,45 +9,40 @@ public class MainOptimizedV2 {
 
     private static int n;
     private static int VERTEX_COUNT;
-    private static int EDGE_COUNT;
     private static Stack<Integer> currPath;
     private static List<Integer> circuit;
     private static int[] edgeCount;
-    private static int[][] adjacenceList;
     private static Map<Integer, Stack<Integer>> adjList;
+    private static StringBuilder solution = new StringBuilder();
+    private static boolean[] visited;
 
     static {
-        n = 3; //sol is 0 1 2 5 3 7 6 4 => hamiltonian path
+        init();
+    }
+
+    private static void init() {
+        n = 4;
         VERTEX_COUNT = 2 << (n - 1);
-        EDGE_COUNT = 2 << n;
         currPath = new Stack<>();
         currPath.push(0);
         circuit = new ArrayList<>();
         edgeCount = new int[VERTEX_COUNT];
         Arrays.fill(edgeCount, 2);
-        adjacenceList = new int[EDGE_COUNT][2];
-        int j = 0;
-        for (int i = 0; i < VERTEX_COUNT; ++i) {
-            adjacenceList[j][0] = i;
-            adjacenceList[j][1] = 2 * (i % (VERTEX_COUNT >> 1));
-            adjacenceList[j + 1][0] = i;
-            adjacenceList[j + 1][1] = adjacenceList[j][1] + 1;
-            j += 2;
-        }
         adjList = new HashMap<>();
-        for(int i = 0; i < VERTEX_COUNT; ++i) {
+        for (int i = 0; i < VERTEX_COUNT; ++i) {
             int neighbourVertex = 2 * (i % (VERTEX_COUNT >> 1));
             Stack<Integer> crtStack = adjList.getOrDefault(i, new Stack<>());
             crtStack.push(neighbourVertex);
             crtStack.push(neighbourVertex + 1);
             adjList.put(i, crtStack);
         }
+        visited = new boolean[VERTEX_COUNT];
     }
 
     private static void eulerianPath() {
         int crtVertex = 0;
 
-        while(!currPath.isEmpty()) {
+        while (!currPath.isEmpty()) {
             if (edgeCount[crtVertex] > 0) {
                 currPath.push(crtVertex);
                 int nextVertex = adjList.get(crtVertex).pop();
@@ -59,140 +53,56 @@ public class MainOptimizedV2 {
                 crtVertex = currPath.pop();
             }
         }
+
+        printEulerianCycle();
     }
 
-    //*************************************************************************
+    private static void deBrujinSequenceGenerator() {
+        int n0, n1;
+        int crtVertex = 0;
 
-    static private ArrayList<Integer>[] adj;
-
-    // A recursive function used by topologicalSort
-    static void topologicalSortUtil(int v, boolean visited[], Stack<Integer> stack)
-    {
-        // Mark the current node as visited.
-        visited[v] = true;
-        Integer i;
-
-        // Recur for all the vertices adjacent to this
-        // vertex
-        for (Integer integer : adj[v]) {
-            i = integer;
-            if (!visited[i])
-                topologicalSortUtil(i, visited, stack);
-        }
-
-        // Push current vertex to stack which stores result
-        stack.push(v);
-    }
-
-    // The function to do Topological Sort. It uses
-    // recursive topologicalSortUtil()
-    static void topologicalSort()
-    {
-        Stack<Integer> stack = new Stack<>();
-
-        // Mark all the vertices as not visited
-        boolean visited[] = new boolean[VERTEX_COUNT];
-        adj = new ArrayList[VERTEX_COUNT];
-        for(int i = 0; i < VERTEX_COUNT; ++i) {
-            adj[i] = new ArrayList<>();
-        }
-
-        // Call the recursive helper function to store
-        // Topological Sort starting from all vertices
-        // one by one
-        for (int i = 0; i < VERTEX_COUNT; i++)
-            if (!visited[i])
-                topologicalSortUtil(i, visited, stack);
-
-        // Print contents of stack
-        while (!stack.empty())
-            System.out.print(stack.pop() + " ");
-    }
-    //*************************************************************************
-
-    /** asdasdasdasdasd **/
-
-    private static void hamiltonianPath() {
-        //sol is 0 1 2 5 3 7 6 4 => hamiltonian path
-
-        System.out.print("0 -> 1 -> ");
-        int crtVertex = 1;
-        boolean maxWasTouched = false;
-
-        while(crtVertex != 0) {
-            if(crtVertex == VERTEX_COUNT - 1) maxWasTouched = true;
-            boolean lastDigitIsZero = (adjacenceList[crtVertex][0] & 1) == 0;
-            if(maxWasTouched) {
-                if(lastDigitIsZero) {
-                    crtVertex = adjacenceList[crtVertex][0];
-                } else {
-                    crtVertex = adjacenceList[crtVertex][1];
-                }
+        while (!currPath.isEmpty()) {
+            n0 = 2 * (crtVertex % (VERTEX_COUNT >> 1));
+            n1 = n0 + 1;
+            if (visited[n0] && visited[n1]) {
+                solution.append(crtVertex & 1);
+                crtVertex = currPath.pop();
             } else {
-                if(lastDigitIsZero) {
-                    crtVertex = adjacenceList[crtVertex][1];
+                currPath.push(crtVertex);
+                if (!visited[n0]) {
+                    crtVertex = n0;
+                    visited[n0] = true;
                 } else {
-                    crtVertex = adjacenceList[crtVertex][0];
+                    crtVertex = n1;
+                    visited[n1] = true;
                 }
             }
-            System.out.print(crtVertex + " -> ");
         }
 
+        printDeBrujinSequence();
     }
 
-    private static void hamiltonianPath2() {
-
-        System.out.print("000 -> ");
-
-        int crtVertex = 1;
-        boolean maxWasTouched = false;
-
-        while(crtVertex != 0) {
-
-            System.out.print(Integer.toBinaryString(crtVertex) + " -> ");
-
-            if(crtVertex == VERTEX_COUNT - 1) {
-                maxWasTouched = true;
-            }
-
-            int n1 = adjList.get(crtVertex).pop();
-            int n2 = adjList.get(crtVertex).pop();
-
-            boolean n1LastDigitIsZero = (n1 & 1) == 0;
-
-            if(maxWasTouched) {
-                crtVertex = n1LastDigitIsZero ? n1 : n2;
-            } else {
-                crtVertex = n1LastDigitIsZero ? n2 : n1;
-            }
-        }
-
-    }
-
-    /** asdasdasdasdasd **/
-
-
-    private static void showAdjList() {
-        IntStream.range(0, EDGE_COUNT).forEach(i -> {
-            System.out.println(String.format("[%d, %d]", adjacenceList[i][0], adjacenceList[i][1]));
-        });
-    }
-
-    private static void printSol() {
-        for (int i=circuit.size()-1; i>=0; i--) {
+    private static void printEulerianCycle() {
+        for (int i = circuit.size() - 1; i >= 0; i--) {
             System.out.print(circuit.get(i) + " ");
-            if(i > 0) {
+            if (i > 0) {
                 System.out.print(" -> ");
             }
         }
+        System.out.println();
+    }
+
+    private static void printDeBrujinSequence() {
+        for (int i = 0; i < n - 2; ++i) {
+            System.out.print(0);
+        }
+        System.out.print(solution.reverse());
     }
 
     public static void main(String[] args) {
-//        showAdjList();
-        //eulerianPath();
-        //printSol();
-//        topologicalSort();
-        hamiltonianPath2();
+        eulerianPath();
+        init();
+        deBrujinSequenceGenerator();
     }
 
 }
